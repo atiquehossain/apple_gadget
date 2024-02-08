3436e3bbafe8616efd160d3611a2876fa2591dea
atique.hossain <nsuatique@gmail.com>
2024-02-08 12:34:21 -0500

    comment

--- lib/services/ApiConstants.dart
+++ lib/services/ApiConstants.dart
@@ -1,5 +1,4 @@
 class ApiConstants {
   static const baseUrl = "https://peanut.ifxdb.com/api/ClientCabinetBasic/";
-//  static const requiresAuthToken = "requiresAuthToken";
- // static const String splashValue = "1";
+
 }
